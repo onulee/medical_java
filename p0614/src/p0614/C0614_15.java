04fa64ac0e03c56df93c21e92cfded5548718b57
onulee <onulee@naver.com>
2024-06-14 18:14:58 +0900

    연산자

--- p0614/src/p0614/C0614_15.java
+++ p0614/src/p0614/C0614_15.java
@@ -3,7 +3,7 @@ package p0614;
 public class C0614_15 {
 
 	public static void main(String[] args) {
-		System.out.println((float)0.1d==0.1f);
+		System.out.println((float)0.1000000d==0.1000000f);
 		double num = 0.1d;
 		System.out.println("double타입 : "+num);
 		

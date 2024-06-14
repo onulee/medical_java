a83075e754a142d89fe34c40f446d08e78e09f7f
onulee <onulee@naver.com>
2024-06-14 18:14:49 +0900

    연산자

---
+++ p0614/src/p0614/C0614_15.java
@@ -0,0 +1,15 @@
+package p0614;
+
+public class C0614_15 {
+
+	public static void main(String[] args) {
+		System.out.println((float)0.1d==0.1f);
+		double num = 0.1d;
+		System.out.println("double타입 : "+num);
+		
+		double d = (double)0.1f;
+		System.out.println("float타입변경 : "+d);
+
+	}
+
+}
